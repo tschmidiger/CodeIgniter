578d641a7cf9016add84ffb39872e951f84f119d
tschmidiger <Thomas.Schmidiger@webber.ch>
2015-08-13 23:41:56 +0200

    setting correct foundation.min.js path

--- application/views/admin/footer.php
+++ application/views/admin/footer.php
@@ -2,7 +2,7 @@
     </div>
     
     
-    <script src="/assets/admin/foundation/bower_components/foundation/js/foundation.min.js"></script>
+    <script src="/assets/admin/foundation/js/foundation.min.js"></script>
     <script src="/assets/admin/foundation/js/app.js"></script>
   </body>
 </html>
